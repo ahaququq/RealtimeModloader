42bc3e008af54e82128c5ac05c41c0f82c7cdc7e
ahaququq <83945944+ahaququq@users.noreply.github.com>
2026-02-25 08:40:21 +0100

    changes

--- hostMod/src/main/kotlin/io/github/ahaququq/realtime_modloader/core/Mod.kt
+++ hostMod/src/main/kotlin/io/github/ahaququq/realtime_modloader/core/Mod.kt
@@ -2,8 +2,7 @@ package io.github.ahaququq.realtime_modloader.core
 
 import kotlinx.io.IOException
 import kotlinx.serialization.SerializationException
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.*
 import java.io.File
 import java.net.URLClassLoader
 
@@ -18,10 +17,11 @@ class Mod {
 	var status = ""
 
 	var classLoader: URLClassLoader? = null
-	var metadataJson: JsonElement? = null
+	var metadataVersion: Int? = null
+
 	var entrypoints = mutableListOf<Entrypoint>()
 
-	data class Entrypoint(val className: String)
+	class Entrypoint(val className: String)
 
 	constructor(file: File) {
 		jarFile = file
@@ -38,21 +38,18 @@ class Mod {
 		classLoader ?: throw ModloadingException("Class loader is (somehow) null")
 
 		val metadataUrl = classLoader!!.findResource(METADATA_PATH)
-
 		if (metadataUrl == null) {
 			status = "Missing metadata (realtime.mod.json)"
 			return
 		}
 
 		val metadataFile = File(metadataUrl.toURI())
-
 		if (!metadataFile.canRead()) {
 			status = "Can't read metadata"
 			return
 		}
 
 		var metadataString = """{}"""
-
 		try {
 			val stream = metadataFile.inputStream()
 			metadataString = String(stream.readAllBytes())
@@ -63,12 +60,20 @@ class Mod {
 		}
 
 		try {
-			metadataJson = Json.parseToJsonElement(metadataString)
+			var metadataJson = Json.parseToJsonElement(metadataString).jsonObject
+			metadataVersion = metadataJson["version"]?.jsonPrimitive?.intOrNull
+			val entryList = metadataJson["entrypoints"]?.jsonArray
+			entryList?.forEach { element ->
+				val value = element.jsonPrimitive.contentOrNull
+
+			}
+
 		} catch (e: SerializationException) {
 			status = "Failed to load metadata: \n${e.stackTraceToString()}"
 			return
+		} catch (e: IllegalArgumentException) {
+			status = "Malformed metadata file: \n${e.stackTraceToString()}"
+			return
 		}
-
-
 	}
 }
